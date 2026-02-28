60632052a627566204d5548de31efeffc0cbab5c
Elparadisogonzalo <azehagowa@elparadisogonzalo.com>
2026-02-28 03:03:33 +0100

    Delete packages directory

--- packages/packages/frontend/src/web3.js
+++
@@ -1,5 +0,0 @@
-import { InjectedConnector } from '@web3-react/injected-connector';
-
-export const injected = new InjectedConnector({
-  supportedChainIds: [1, 56, 137, 80001], // Ethereum, BSC, Polygon, Mumbai
-});
