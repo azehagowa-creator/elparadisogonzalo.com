10dd7c7216828003f9aea985992d3dfd495b71c5
azehagowa-creator <azehagowa@gmail.com>
2026-07-14 19:12:17 +0000

    approve frontend deletions

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.elparadisogonzalo.app',
-  appName: 'elparadisogonzalo app',
-  webDir: 'build'
-};
-
-export default config;
