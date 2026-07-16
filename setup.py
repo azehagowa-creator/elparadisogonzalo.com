8643811d2648f35fe9c9f1dbb7f7840888e7352a
azehagowa-creator <azehagowa@gmail.com>
2026-07-16 23:16:26 +0000

    approve

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="elparadisogonzalo",
+    version="2.0.0",
+    description="Elparadisogonzalo Python package",
+    author="azehagowa-creator",
+    author_email="genyoungclip@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.13",
+)
