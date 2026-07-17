e2d74a5d5a99e2836cb8191fd96000d085bedff2
azeh agowa <azehagowa@gmail.com>
2026-07-17 21:40:13 +0100

    Delete .elparadisogonzalo directory

--- .elparadisogonzalo/js/decode.js
+++
@@ -1,5 +0,0 @@
-const contentHash = require('content-hash');
-
-const encoded = '0x93f028255035b61df476b13b9dba3c4f06f60e51b9b4caee31680b389aef327f';
-const decoded = contentHash.decode(encoded);
-console.log('Decoded:', decoded);
