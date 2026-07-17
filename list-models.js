0fc85725c1d5d359c43c386533e7e61ac0ab4358
azeh agowa <azehagowa@gmail.com>
2026-07-17 23:05:21 +0100

    Delete list-models.js

--- list-models.js
+++
@@ -1,7 +0,0 @@
-import openai from "./config/openai.config.js";
-
-const models = await openai.models.list();
-
-for (const model of models.data.sort((a, b) => a.id.localeCompare(b.id))) {
-  console.log(model.id);
-}
